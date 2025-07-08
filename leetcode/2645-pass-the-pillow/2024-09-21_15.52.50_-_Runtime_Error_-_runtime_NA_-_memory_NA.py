b235b19bb75ee2896093e6347c5c33b98ddf9794
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:36:16 -0700

    Add solution for 2645-pass-the-pillow

---
+++ leetcode/2645-pass-the-pillow/2024-09-21_15.52.50_-_Runtime_Error_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,15 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        # original n range + without the edges of n so it can be cyclic
+        new = [i for i in range(1, n+1)] + [i for i in range(n-1,1,-1)]
+        cycle = len(new)
+
+        while time > cycle or time > n:
+            time -= cycle
+
+        return new[time]
+
+        
+
+
+
