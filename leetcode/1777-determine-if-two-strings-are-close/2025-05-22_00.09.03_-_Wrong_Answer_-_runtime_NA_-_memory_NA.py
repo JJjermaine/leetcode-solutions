e12c586cddebe6fb948dc7478f32dd9a23caf320
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:28:10 -0700

    Add solution for 1777-determine-if-two-strings-are-close

---
+++ leetcode/1777-determine-if-two-strings-are-close/2025-05-22_00.09.03_-_Wrong_Answer_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,14 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        # conditions must hold
+        # all letters in word1 must appear in word2
+        # each count of unique letters in word1 are 1:1 with word2
+        d1 = Counter(word1)
+
+        d2 = Counter(word2)
+
+        for (k1, v1), (k2, v2) in zip(d1.items(), d2.items()):
+            if not d2[k1] or v1 != v2:
+                return False
+        return True
+
