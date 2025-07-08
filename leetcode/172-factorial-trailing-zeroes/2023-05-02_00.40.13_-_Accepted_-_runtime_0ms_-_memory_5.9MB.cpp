e03f2b4c1789a77a53c267506edf7b14f16c9f99
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:27:41 -0700

    Add solution for 172-factorial-trailing-zeroes

---
+++ leetcode/172-factorial-trailing-zeroes/2023-05-02_00.40.13_-_Accepted_-_runtime_0ms_-_memory_5.9MB.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int total = 0;
+        while (n > 0) {
+            total += n / 5;
+            n /= 5;
+        }
+        return total;
+    }
+};
