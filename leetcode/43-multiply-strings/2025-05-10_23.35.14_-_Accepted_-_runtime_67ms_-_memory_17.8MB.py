55524cf8983c44db6bd537dd3b04eaa05682a632
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:22:14 -0700

    Add solution for 43-multiply-strings

---
+++ leetcode/43-multiply-strings/2025-05-10_23.35.14_-_Accepted_-_runtime_67ms_-_memory_17.8MB.py
@@ -0,0 +1,18 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        n = len(num1)
+        m = len(num2)
+        res = [0] * (len(num1) + len(num2))
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                res[i + j + 1] += int(num1[i]) * int(num2[j])
+
+                res[i + j] += res[i + j + 1] // 10
+                res[i + j + 1] %= 10
+        while res[0] == 0:
+            res.pop(0)
+        return "".join(map(str, res))
+                        
+        
