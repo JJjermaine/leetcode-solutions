e2f550823635f90bc6758a5e031ce530306a0da7
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:17:51 -0700

    Add solution for 950-x-of-a-kind-in-a-deck-of-cards

---
+++ leetcode/950-x-of-a-kind-in-a-deck-of-cards/2024-09-22_22.08.01_-_Runtime_Error_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,23 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        d = {}
+        for num in deck:
+            if num not in d:
+                d[num] = 1
+            else:
+                d[num] += 1
+
+        
+        min_division = min(d.values()) if min(d.values) % 2 != 0 else 2
+        if min_division == 1:
+            return False
+
+        for key, value in d.items():
+            while value:
+                value -= min_division
+                if value < 0:
+                    return False
+
+        return True
+
+        
