6cc495f6c748de98b0351d8524567bc60e07af40
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:35:58 -0700

    Add solution for 2634-minimum-common-value

---
+++ leetcode/2634-minimum-common-value/2024-09-27_21.43.27_-_Wrong_Answer_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        p1, p2 = 0, 0
+        res = -1
+        while p1 < len(nums1)-1 and p2 < len(nums2)-1:
+            if nums1[p1] < nums2[p2]:
+                p1 += 1
+            elif nums1[p1] > nums2[p2]:
+                p2 += 1
+            else:
+                res = nums1[p1]
+                break
+
+        return res
+        
