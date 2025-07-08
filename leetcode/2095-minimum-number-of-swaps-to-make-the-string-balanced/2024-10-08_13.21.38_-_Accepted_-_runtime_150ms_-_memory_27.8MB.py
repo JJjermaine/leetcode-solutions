f0c2a976a8589e2d5bb6315832fda4d4da00521a
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:30:34 -0700

    Add solution for 2095-minimum-number-of-swaps-to-make-the-string-balanced

---
+++ leetcode/2095-minimum-number-of-swaps-to-make-the-string-balanced/2024-10-08_13.21.38_-_Accepted_-_runtime_150ms_-_memory_27.8MB.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        # ans not my own
+        res = 0
+        for c in s:
+            if c == "[":
+                res += 1
+
+            elif res > 0:
+                res -= 1
+
+        return ((res+1)//2)
+            
+
+        
+        
