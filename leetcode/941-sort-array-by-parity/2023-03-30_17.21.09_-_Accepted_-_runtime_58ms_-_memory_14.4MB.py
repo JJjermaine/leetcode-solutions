8491b7661baa79758cce499940d5ef07e80be8a8
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:18:03 -0700

    Add solution for 941-sort-array-by-parity

---
+++ leetcode/941-sort-array-by-parity/2023-03-30_17.21.09_-_Accepted_-_runtime_58ms_-_memory_14.4MB.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        a = []
+        for i in nums:
+            if (i % 2 == 0):
+                a.append(i)
+        
+        for i in nums:
+            if (i % 2 != 0):
+                a.append(i)
+
+        return a
