536e5f09b2c67a424043905b0393b6e47a8cd256
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:20:11 -0700

    Add solution for 680-valid-palindrome-ii

---
+++ leetcode/680-valid-palindrome-ii/2023-04-01_01.27.32_-_Accepted_-_runtime_120ms_-_memory_14.2MB.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                # Check if deleting the left character makes the rest of the string a palindrome
+                if self.isPalindrome(s, left + 1, right):
+                    return True
+                # Check if deleting the right character makes the rest of the string a palindrome
+                elif self.isPalindrome(s, left, right - 1):
+                    return True
+                else:
+                    # Both substrings are not palindromes
+                    return False
+            left += 1
+            right -= 1
+        return True
+    
+    def isPalindrome(self, s, left, right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
