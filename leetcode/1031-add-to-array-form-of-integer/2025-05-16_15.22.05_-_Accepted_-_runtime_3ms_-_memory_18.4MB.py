f944ca6d9ccbe0b14dd54b82889a531d42af0f24
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:22:54 -0700

    Add solution for 1031-add-to-array-form-of-integer

---
+++ leetcode/1031-add-to-array-form-of-integer/2025-05-16_15.22.05_-_Accepted_-_runtime_3ms_-_memory_18.4MB.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        k = str(k)
+        longest = max(len(num), len(k)) + 1
+        while len(num) != longest:
+            num.insert(0,0)
+        j = len(num)-1
+        for i in range(len(k)-1,-1,-1):
+            num[j] += int(k[i])
+            j -= 1
+
+        for i in range(len(num)-1,-1,-1):
+            if num[i] > 9:
+                num[i-1] += num[i] // 10
+                num[i] %= 10
+            
+        while num[0] == 0:
+            num.pop(0)
+        return num
+        
