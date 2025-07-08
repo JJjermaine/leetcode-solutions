97406f89915916f54d24cc45d89e810fd4117aff
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:21:52 -0700

    Add solution for 482-license-key-formatting

---
+++ leetcode/482-license-key-formatting/2025-05-27_14.56.45_-_Wrong_Answer_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,18 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        n = len(s)
+        res = ""
+        curr = 0
+        for i in range(n-1,-1,-1):
+            if curr == k:
+                res += "-"
+                curr = 0
+            if s[i].isalpha():
+                res += s[i].upper()
+                curr += 1
+            elif s[i].isnumeric():
+                res += s[i]
+                curr += 1
+        return res[::-1]
+
+
