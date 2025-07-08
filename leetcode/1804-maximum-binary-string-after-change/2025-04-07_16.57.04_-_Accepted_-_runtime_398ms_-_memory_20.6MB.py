1b730c05512aa4d18b32ff423e154751c6d07a54
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:28:27 -0700

    Add solution for 1804-maximum-binary-string-after-change

---
+++ leetcode/1804-maximum-binary-string-after-change/2025-04-07_16.57.04_-_Accepted_-_runtime_398ms_-_memory_20.6MB.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maximumBinaryString(self, binary: str) -> str:
+        # move all 1's  to the right and make all 00 to 10
+        res = []
+        ones = 0
+        leading = 0
+        i = 0
+        # count leading 1's as an edge case
+        for num in binary:
+            if num == "0":
+                break
+            else:
+                res.append("1")
+                leading += 1
+
+        for num in binary:
+            if num == "0":
+                res.append("0")
+            else:
+                ones += 1
+        
+        for i in range(ones - leading):
+            res.append("1")
+
+
+        for i in range(len(binary)-1):
+            if res[i] == "0" and res[i+1] == "0":
+                res[i], res[i+1] = "1", "0"
+
+        return "".join(res)
+        
+                
+
+        
