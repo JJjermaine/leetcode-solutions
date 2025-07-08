4c788e0ccb02dc1618d6acb15a8ea4b9c5f2739f
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:36:21 -0700

    Add solution for 2646-kth-largest-sum-in-a-binary-tree

---
+++ leetcode/2646-kth-largest-sum-in-a-binary-tree/2024-10-21_19.03.01_-_Runtime_Error_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        # perform bfs to calculate sum for each level and append it to array
+        # sort the array and return the kth largest
+        arr = []
+        queue = deque([root])
+
+        # bfs
+        while len(queue) > 0:
+            count = 0
+            for i in range(len(queue)):
+                node = queue.popleft()
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+                count += node.val
+
+            arr.append(count)
+
+        arr.sort()
+        return arr[-k]
+        
