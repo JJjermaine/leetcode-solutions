d320762382b99bcc06de84af98fff3123807396a
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:21:03 -0700

    Add solution for 598-range-addition-ii

---
+++ leetcode/598-range-addition-ii/2024-09-23_17.51.42_-_Memory_Limit_Exceeded_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        matrix = [[0 for _ in range(n)] for _ in range(m)]
+
+        for i in range(len(ops)):
+            
+            x, y = ops[i][0], ops[i][1]
+        
+            for i in range(x):
+                for j in range(y):
+                    matrix[i][j] += 1
+
+        count = {}
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] not in count:
+                    count[matrix[i][j]] = 1
+                else:
+                    count[matrix[i][j]] += 1
+
+        if count:
+            max_key = max(count.keys())
+            max_value = count[max_key]
+
+        return max_value
+        
