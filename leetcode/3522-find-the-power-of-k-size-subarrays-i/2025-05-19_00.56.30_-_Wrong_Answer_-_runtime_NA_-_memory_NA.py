98a6a01871c34be2452b57eed80c31c0ebaf53fb
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:43:24 -0700

    Add solution for 3522-find-the-power-of-k-size-subarrays-i

---
+++ leetcode/3522-find-the-power-of-k-size-subarrays-i/2025-05-19_00.56.30_-_Wrong_Answer_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,25 @@
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        # sliding window
+        n = len(nums)
+        if n == 1 or k == 1:
+            return nums
+        res = []
+        satisfy = 1
+        for i in range(1, n):
+            if nums[i-1] + 1 == nums[i]:
+                satisfy += 1
+                if satisfy > k:
+                    satisfy -= 1
+            else:
+                satisfy -= 1
+                if satisfy == 0:
+                    satisfy = 1
+            if i+1 >= k:
+                if satisfy == k:
+                    res.append(nums[i])
+                else:
+                    res.append(-1)
+        return res
+        
+
