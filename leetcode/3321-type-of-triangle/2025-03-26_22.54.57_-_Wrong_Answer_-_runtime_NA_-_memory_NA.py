733317ac19979581451c6404bff6d6a4860e6e85
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:41:29 -0700

    Add solution for 3321-type-of-triangle

---
+++ leetcode/3321-type-of-triangle/2025-03-26_22.54.57_-_Wrong_Answer_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,31 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        if nums[0] == nums[1] == nums[2]:
+            return "equilateral"
+
+
+        largest = max(nums)
+        squared = 0
+        for i in range(len(nums)):
+            if nums[i] != largest:
+                squared += nums[i] * nums[i]
+        if squared == largest * largest:
+            return "scalene"
+
+        if nums[0] == nums[1] or nums[1] == nums[2] or nums[0] == nums[2]:
+            smallest = min(nums)
+            largest = max(nums)
+            sc = 0
+            lc = 0
+            for num in nums:
+                if num == smallest:
+                    sc += 1
+                else:
+                    lc += 1
+            
+            if largest < smallest * 2 and sc == 2 or largest * 2 > smallest and lc == 2:
+                return "isosceles"
+
+        return "none"
+
+        
