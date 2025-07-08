ca37fbd80cd97970a3f5530ecd066f19b445c969
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:20:37 -0700

    Add solution for 643-maximum-average-subarray-i

---
+++ leetcode/643-maximum-average-subarray-i/2024-10-18_22.28.30_-_Wrong_Answer_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        res = -100000
+        curr = 0
+        l = 0
+        for r in range(len(nums)):
+            curr += nums[r]
+            while r - l >= k:
+                curr -= nums[l]
+                l += 1
+            res = max(res, curr / k)
+
+
+        return res
+
+            
+        
