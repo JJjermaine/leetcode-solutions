521f1d3224652c0f2c2e74f7673d1314139b03d5
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:29:51 -0700

    Add solution for 2005-check-if-all-the-integers-in-a-range-are-covered

---
+++ leetcode/2005-check-if-all-the-integers-in-a-range-are-covered/2025-04-19_23.18.54_-_Runtime_Error_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        arr = [0] * 50
+        for l, r in ranges:
+            for i in range(l, r+1):
+                arr[i] = 1
+        for i in range(left, right+1):
+            if arr[i] == 1:
+                return True
+        
+        return False
+        
