39b284768f9129822bacd72ccc85ff0005253b03
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:25:14 -0700

    Add solution for 14-longest-common-prefix

---
+++ leetcode/14-longest-common-prefix/2025-01-21_19.19.17_-_Wrong_Answer_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # naive solution, get first letter and extend it and keep matching
+        if "" in strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+
+        longest = "" # string
+        longest_i = 0
+        i = 0
+        while True:
+            if longest != strs[i][:longest_i]:
+                return strs[0][:longest_i-1]
+            if i == len(strs) - 1: # reached the end of list, extend longest
+                i = 0
+                longest_i += 1
+                longest = strs[0][:longest_i]
+                if longest_i == len(strs[0]):
+                    return longest
+            i += 1
+        return longest
+
+
+        
