0c5cfd16f15de2546822fb1764dd304ae61115a8
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:40:12 -0700

    Add solution for 3188-find-champion-i

---
+++ leetcode/3188-find-champion-i/2024-09-27_22.11.54_-_Accepted_-_runtime_438ms_-_memory_17MB.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findChampion(self, grid: List[List[int]]) -> int:
+        count = {}
+        for i in range(len(grid)):
+            wins = 0
+            for j in range(len(grid)):
+                wins += (grid[i][j])
+
+            count[i] = wins
+
+        return max(count, key=count.get)
+
+                
+        
