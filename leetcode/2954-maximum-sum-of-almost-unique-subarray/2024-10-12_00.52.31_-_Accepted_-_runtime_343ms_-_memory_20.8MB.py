9399d602a4cfb1eb5c46a2680965d8a3c345d354
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:39:04 -0700

    Add solution for 2954-maximum-sum-of-almost-unique-subarray

---
+++ leetcode/2954-maximum-sum-of-almost-unique-subarray/2024-10-12_00.52.31_-_Accepted_-_runtime_343ms_-_memory_20.8MB.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxSum(self, nums: List[int], m: int, k: int) -> int:
+         # ans is my own & looked at hints
+        count = {}
+        # add first k elements to dictionary
+        current_sum = 0
+        for i in range(k):
+            count[nums[i]] = count.get(nums[i], 0) + 1
+            current_sum += nums[i]
+
+        res = 0
+        for i in range(k, len(nums)):
+            # just slide the window if less than k distinct elements
+            if len(count) >= m:
+                res = max(res, current_sum)
+            # delete old side of window
+            count[nums[i-k]] -= 1
+            current_sum -= nums[i-k]
+            if count[nums[i-k]] == 0:
+                del count[nums[i-k]] 
+
+            # add new side of window
+            count[nums[i]] = count.get(nums[i], 0) + 1
+            current_sum += nums[i]
+
+        # loop didnt sum the last k characters so perform here
+        if len(count) >= m:
+            res = max(res, current_sum)
+
+        return res
