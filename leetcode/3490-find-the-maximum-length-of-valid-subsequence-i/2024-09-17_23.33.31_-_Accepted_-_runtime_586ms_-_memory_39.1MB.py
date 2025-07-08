acce58e134be596bcee540363f7cf803e5c5657d
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:43:04 -0700

    Add solution for 3490-find-the-maximum-length-of-valid-subsequence-i

---
+++ leetcode/3490-find-the-maximum-length-of-valid-subsequence-i/2024-09-17_23.33.31_-_Accepted_-_runtime_586ms_-_memory_39.1MB.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        # greedy solution, go through all odd/even, odd/odd,  even/odd, even/even parities
+        c = nums[0] % 2
+        odd = 0
+        even = 0
+        both = 0
+        for num in nums:
+            if num % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+
+            if num % 2 == c:
+
+                both += 1
+                c = 1 - c
+   
+        return max(even, odd, both)
