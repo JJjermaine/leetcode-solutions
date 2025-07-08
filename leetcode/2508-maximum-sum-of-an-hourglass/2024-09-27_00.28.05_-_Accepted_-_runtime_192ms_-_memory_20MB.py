d68fd5d8aa153703f200e1283a1316b4f511b7c2
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:34:29 -0700

    Add solution for 2508-maximum-sum-of-an-hourglass

---
+++ leetcode/2508-maximum-sum-of-an-hourglass/2024-09-27_00.28.05_-_Accepted_-_runtime_192ms_-_memory_20MB.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        max_sum = 0
+        for row in range(len(grid)-2):
+            for col in range(len(grid[0])-2):
+                # top, middle, and bottom
+                top = grid[row][col] + grid[row][col+1] + grid[row][col+2]
+                middle = grid[row+1][col+1]
+                bottom = grid[row+2][col] + grid[row+2][col+1] + grid[row+2][col+2]
+                max_sum = max((top + middle + bottom), max_sum)
+
+        return max_sum
+        
