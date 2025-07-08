9a1c651ea8149039171204ca7dff3f62e5125d64
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 01:18:30 -0700

    Add solution for 876-hand-of-straights

---
+++ leetcode/876-hand-of-straights/2025-05-09_23.34.10_-_Accepted_-_runtime_63ms_-_memory_19.1MB.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if groupSize == 1:
+            return True
+        hand.sort()
+        d = Counter(hand)
+
+        while d:
+            first = next(iter(d))
+            for i in range(groupSize):
+                if not d[first+i]:
+                    return False
+                d[first+i] -= 1
+                if d[first+i] == 0:
+                    del d[first+i]
+
+        return True
