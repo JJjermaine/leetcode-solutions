9eaed3b8fd9199ee364f35d7b6bf028e1525bf9a
JJjermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-08 00:29:34 -0700

    Add solution for 198-house-robber

---
+++ leetcode/198-house-robber/2025-04-25_13.18.32_-_Accepted_-_runtime_0ms_-_memory_18MB.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # dp, option where you do rob vs skip
+        n = len(nums)
+        # edge case, n == 1
+        if n == 1:
+            return nums[0]
+
+        # edge case, n == 2
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        # edge case, n == 3
+        if n == 3:
+            return max(nums[0] + nums[2], nums[1])
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        dp[2] = nums[2] + nums[0]
+        for i in range(3, n):
+            dp[i] = max(nums[i] + max(dp[i-2], dp[i-3]),  dp[i-1])
+        return dp[-1]
