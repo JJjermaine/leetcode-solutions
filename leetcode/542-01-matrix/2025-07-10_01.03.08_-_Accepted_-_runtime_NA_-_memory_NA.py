6aa3c117fcc927aa0e403933c75ad86f5fdbaa7b
Jermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-10 01:03:09 -0700

    Add solution for 542 01 matrix

---
+++ leetcode/542-01-matrix/2025-07-10_01.03.08_-_Accepted_-_runtime_NA_-_memory_NA.py
@@ -0,0 +1,25 @@
+                        mat[i][j] = min(mat[i][j], mat[i][j-1] + 1)
+
+        for i in range(rows-1,-1,-1):
+            for j in range(cols-1,-1,-1):
+                if mat[i][j] != 0:
+                    if i < rows-1:
+                        mat[i][j] = min(mat[i][j], mat[i+1][j] + 1)
+                        mat[i][j] = min(mat[i][j], mat[i-1][j] + 1)
+                    if j > 0:
+                    if i > 0:
+                if mat[i][j] != 0:
+            for j in range(cols):
+        for i in range(rows):
+
+            for j in range(cols):
+                if mat[i][j] == 1:
+                    mat[i][j] = float('inf')
+        for i in range(rows):
+        cols = len(mat[0])
+                    if j < cols-1:
+                        mat[i][j] = min(mat[i][j], mat[i][j+1] + 1)
+        return mat
+        rows = len(mat)
+
+        # min of the surrounding + 1
