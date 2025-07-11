30c4583b2e81aca5f6391551fe85f2fb3721d8f6
Jermaine <43556451+JJjermaine@users.noreply.github.com>
2025-07-11 11:59:35 -0700

    Add solution for Non-overlapping Intervals

---
+++ leetcode/435-non-overlapping-intervals/2025-07-11_11.59.24_-_Accepted_-_runtime_102_ms_-_memory_51.4_MB.py
@@ -0,0 +1,13 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # greedily remove the largest overlapping intervals
+        intervals.sort(key=lambda x: x[1])
+        n = len(intervals)
+        prev = 0
+        count = 1
+        for i in range(1, n):
+            if intervals[i][0] >= intervals[prev][1]:
+                prev = i
+                count += 1
+
+        return n - count
