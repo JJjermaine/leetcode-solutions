78ea7cf6c9e33da89809688bf7b44bf2583dc5d6
Jermaine <43556451+JJjermaine@users.noreply.github.com>
2025-09-03 20:30:37 -0700

    Add solution for Coin Change

---
+++ leetcode/322-coin-change/2025-09-03_20.30.27_-_Accepted_-_runtime_692_ms_-_memory_18.1_MB.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for curr in range(coin, amount + 1):
+                dp[curr] = min(dp[curr], dp[curr - coin] + 1)
+
+        if dp[amount] != float("inf"):
+            return dp[amount]
+        
+        return -1
